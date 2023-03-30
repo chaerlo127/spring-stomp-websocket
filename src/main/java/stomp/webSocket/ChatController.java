8f2d51d517f3e8560703efadbf623c94c2b4ac78
chaerlo127 <monynony0203@gmail.com>
2023-03-31 02:04:09 +0900

    feat: websocket chatController 생성

--- src/main/java/stomp/webSocket/ChatController.java
+++ src/main/java/stomp/webSocket/ChatController.java
@@ -1,9 +1,25 @@
 package stomp.webSocket;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import stomp.webSocket.dto.ChatRoom;
 
-@Controller
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/chat")
 public class ChatController {
+    private final ChatService chatService;
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name){
+        return this.chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRooms(){
+        return chatService.findAllRoom();
+    }
 }
